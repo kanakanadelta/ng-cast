00253766eed4c17fbbf7515c120adfe44bd915cb
kanakanadelta <kanakanadelta@gmail.com>
2018-08-18 11:02:56 -0700

    Started Challenged, implemented keypress and autoplay

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,36 @@
 angular.module('video-player')
 
-.component('app', {
+  .component('app', {
 
-  templateUrl:'src/templates/app.html',  
-  controller: function(youTube){
-  this.searchyoutube = youTube.search;
-  this.videos = window.exampleVideoData;
-  this.dvideo = this.videos[0];
-  this.autoplay = 1;
-
-
-  //ES6 scoping, arrow function binds it to the app.
-  this.changevideo = (value) => {
-    this.dvideo = value;
-  }
-
-  this.searchclick = (value) => {
-    this.searchyoutube(value, this.render)
-  }
-
-  this.toggleauto = () => {
-    console.log('click');
-    if(this.autoplay === 1) {
-      this.autoplay = 0;
-    } else if (this.autoplay ===0) {
+    templateUrl: 'src/templates/app.html',  
+    controller: function(youTube) {
+      this.searchyoutube = youTube.search;
+      this.videos = window.exampleVideoData;
+      this.dvideo = this.videos[0];
       this.autoplay = 1;
-    }
-  }
 
-  this.render = (data) => {
-    this.videos = data.data.items;
-    this.dvideo = data.data.items[0];
-  }
-  }
-});
+
+      //ES6 scoping, arrow function binds it to the app.
+      this.changevideo = (value) => {
+        this.dvideo = value;
+      };
+
+      this.searchclick = (value) => {
+        this.searchyoutube(value, this.render);
+      };
+
+      this.toggleauto = () => {
+        console.log('click');
+        if(this.autoplay === 1) {
+          this.autoplay = 0;
+        } else if (this.autoplay ===0) {
+          this.autoplay = 1;
+        }
+      };
+
+      this.render = (data) => {
+        this.videos = data.data.items;
+        this.dvideo = data.data.items[0];
+      };
+    }
+  });
